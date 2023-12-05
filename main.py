e1bf43e4614bb7664d0681bbdaf81794b0987ff9
chiariniolivia <chiariniolivia@gmail.com>
2023-12-05 09:33:17 -0500

    imported more functions to make fits

--- main.py
+++ main.py
@@ -28,6 +28,42 @@ def plotImg(image, numStd, titleStr):
   plt.tight_layout()
   plt.colorbar()
 
+def extract_radial_data(data, xC, yC):
+    #Get matrix of integer indices associated with subFrame
+    y, x = np.indices((data.shape))
+
+    #Generate matrix of radius values
+    r = np.sqrt((x - xC)**2 + (y - yC)**2)
+
+    #Force integer values (np.sqrt gives floats)
+    r = r.astype(int)
+
+    #Generate a histogram of radius bin, each weighed by corresponding counts
+    weightedRadiusHistogram = np.bincount(r.ravel(), weights=data.ravel())
+    unweightedRadiusHistogram = np.bincount(r.ravel())
+
+    #Get average for each radius bin
+    averageCountsPerRadiusBin = weightedRadiusHistogram / unweightedRadiusHistogram
+    return averageCountsPerRadiusBin
+
+def gaussian_1d(x, mu, sigma, amplitude, offset):
+    #Model function as gaussian with amplitude A and offset G
+    return amplitude * np.exp( -((x-mu)/sigma)**2/2 ) + offset
+
+
+def fit_gaussian_1d(x_data, y_data, p0):
+    # p0 behaves by taking a best guess at params (mu, sigma, amplitude, offset)
+    params, _ = curve_fit(gaussian_1d, x_data, y_data, p0)
+
+    #Calculate coefficient of determination
+    res = y_data - gaussian_1d(x_data, *params)
+    sumSqrs_res = np.sum(res*res)
+    totSumSqrs = np.sum((y_data-np.mean(y_data))**2)
+    R2 = 1.0 - (sumSqrs_res / totSumSqrs)
+
+    return params, R2
+
+
 
 #main script
 
@@ -82,8 +118,6 @@ if __name__ == '__main__':
                 
                 elif(hdul.header["FRAMETYP"]=="Bias"):
                     biasFits.append(hdul.data)
-        
-
 
 
     darkTimes = []
@@ -105,9 +139,8 @@ if __name__ == '__main__':
     masterDarkFlatdict=dict(none)
     #for key in darkDicionary.keys():
     #
-    """
-    check if exposure time is inside list of flat times 
     
     """
+    check if exposure time is inside list of flat times 
 
-
+    """
